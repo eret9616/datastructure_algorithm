f6084ea5e8681adce286882e9032756a155ef1c3
huhao03 <huhao03@kuaishou.com>
2021-06-08 11:49:07 +0800

    .

--- src/LinearSearch.java
+++ src/LinearSearch.java
@@ -1,19 +1,18 @@
-public class LinearSearch {
+public class LinearSearch<E> {
 
     private LinearSearch(){}
 
-    public static int search(int[] data, int target){
+    public static <E> int search(E[] data, E target){
         for(int i =0;i <data.length;i++)
-            if(data[i]== target)
+            if(data[i].equals(target))
                 return i;
-
 
         return -1;
     }
 
-    public  static  void main(String[] args){
+    public  static  void main(String[] args) {
 
-        int[] data ={24,18,12,9,16,66,32,4};
+        Integer[] data ={24,18,12,9,16,66,32,4};
 
         int res = LinearSearch.search(data,16);
 
@@ -21,6 +20,18 @@ public class LinearSearch {
 
         int res2 = LinearSearch.search(data,666);
         System.out.println(res2);
+
+
+        Student[] students = {
+                new Student("Alice"),
+                new Student("Bobo"),
+                new Student("Charles")
+        };
+
+        Student bobo = new Student("Bobo");
+
+        int res3 = LinearSearch.search(students,bobo);
+        System.out.println(res3);
 
     }
 
