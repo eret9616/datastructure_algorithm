3ff4463beb3ea67bc1eb1c975cf5990396fbaf57
huhao03 <huhao03@kuaishou.com>
2021-06-08 14:38:01 +0800

    .

--- src/Student.java
+++ src/Student.java
@@ -7,5 +7,20 @@ public class Student {
     }
 
 
+    @Override
+    public boolean equals(Object student){
+        if(this == student)
+            return true;
+
+        if(student == null)
+            return false;
+
+        if(this.getClass() != student.getClass())
+            return false;
+
+
+        Student another = (Student)student;
+        return this.name.equals(another.name);
+    }
 
 }
